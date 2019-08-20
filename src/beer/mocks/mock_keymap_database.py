979396809427bb17292bba7fb80ee5b7ae562b1f
Ben Coleman <colemanb@moravian.edu>
2019-08-20 15:57:46 -0400

    Add comments and rename variable

--- src/beer/mocks/mock_keymap_database.py
+++ src/beer/mocks/mock_keymap_database.py
@@ -1,12 +1,19 @@
 
 class MockKeyMapDatabase:
+    """
+    In-memory database for testing.  This class has the same interface as keymap_database.py
+    """
 
-    def __init__(self, products):
-        self.products = products
+    def __init__(self, key_map):
+        """
+        Initialize with the desired map
+        :param key_map: the map
+        """
+        self.key_map = key_map
 
     def read_map(self):
-        return self.products
+        return self.key_map
 
     def add(self, product, id):
-        self.products[product] = id
+        self.key_map[product] = id
 
