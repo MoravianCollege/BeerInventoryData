41dffa031540f2fc10e458ccf5860b31eb354cec
Ben Coleman <colemanb@moravian.edu>
2019-08-20 13:14:40 -0400

    Refactor db to parameter

--- src/beer/ingest/keymap.py
+++ src/beer/ingest/keymap.py
@@ -4,13 +4,10 @@ from beer.ingest.keymap_database import KeyMapDatabase
 
 class KeyMap:
 
-    def __init__(self, table):
-        self.connect(table)
+    def __init__(self, keymap_db):
+        self.key_map_db = keymap_db
         self.key_map = self.key_map_db.read_map()
         self.next_id = max(self.key_map.values(), default=0) + 1
-
-    def connect(self, table):
-        self.key_map_db = KeyMapDatabase(table)
 
     def get_value(self, product):
         if product not in self.key_map:
--- tests/test_key_map.py
+++ tests/test_key_map.py
@@ -3,24 +3,14 @@ from beer.ingest.keymap import KeyMap
 from beer.mocks.mock_keymap_database import MockKeyMapDatabase
 
 
-def empty_connect(self, table):
-    self.key_map_db = MockKeyMapDatabase({})
-
-
-def test_get_new_products(mocker):
-    mocker.patch.object(KeyMap, 'connect', empty_connect)
-    p = KeyMap('products')
+def test_get_new_products():
+    p = KeyMap(MockKeyMapDatabase({}))
     assert p.get_value('beer') == 1
     assert p.get_value('grog') == 2
 
 
-def populated_connect(self, table):
-    self.key_map_db = MockKeyMapDatabase({'grog': 1, 'beer': 2})
-
-
-def test_prexisting_products(mocker):
-    mocker.patch.object(KeyMap, 'connect', populated_connect)
-    p = KeyMap('products')
+def test_prexisting_products():
+    p = KeyMap(MockKeyMapDatabase({'grog': 1, 'beer': 2}))
     # grog is 1, beer is 2
     assert p.get_value('beer') == 2
     assert p.get_value('food') == 3
