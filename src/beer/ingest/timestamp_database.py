60ac65aecc56c57b3ebc29185edfe6f0dc0479cd
Ben Coleman <colemanb@moravian.edu>
2019-09-02 20:14:03 -0400

    Cache timestamps

--- src/beer/ingest/timestamp_database.py
+++ src/beer/ingest/timestamp_database.py
@@ -3,6 +3,7 @@ import dotenv
 import os
 import psycopg2
 
+
 class TimestampDatabase:
     """
     Interface to write timestamps to the database
@@ -18,12 +19,24 @@ class TimestampDatabase:
 
         self.conn = psycopg2.connect(dbname=database_name, host=host, user=user, password=password)
         self.cur = self.conn.cursor()
+        self.timestamps = set()
+
+        self.cur.execute('SELECT * FROM timestamps');
+        for timestamp, in self.cur.fetchall():
+            self.timestamps.add(str(timestamp))
 
     def add(self, timestamp):
         """
-        :param timestamp: the timestamp to add
+        :param timestamp: (string) the timestamp to add
         :return: None
         :except: psycopg2.errors.UniqueViolation if the timestamp already exists
         """
+        if self.contains(timestamp):
+            return
+
         self.cur.execute("INSERT INTO timestamps (timestamp) VALUES ('{}');".format(timestamp))
         self.conn.commit()
+        self.timestamps.add(timestamp)
+
+    def contains(self, timestamp):
+        return timestamp in self.timestamps
