46bdfb7d08df651e539cf94662457501ef5fd14b
Ben Coleman <colemanb@moravian.edu>
2019-10-01 21:39:56 -0400

    Use new database interface and compute transactions

--- src/beer/ingest/ingest.py
+++ src/beer/ingest/ingest.py
@@ -1,33 +1,45 @@
-
-
-from beer.ingest.keymap import KeyMap
-from beer.ingest.timestamp_database import TimestampDatabase
-from beer.ingest.convert import Converter
+from beer.ingest.db_transactions import DBTransactions
+from beer.ingest.name_map import NameMap
+from beer.ingest.product_map import ProductMap
+from beer.ingest.current_csv_inventory import CurrentCSVInventory
+from beer.ingest.size_parser import parse_size
+from beer.ingest.transactions import compute_transactions
 import pandas as pd
-import io
 import psycopg2
 
 
 class Ingest:
     """
-    Ingest one CSV file into the Inventory table.  This will convert name, size, and category to
-    integer values form the corresponding tables (adding new entries, if necessary) and
-    truncate the fractional part of the timestamp.
+    Ingest one CSV file into the Inventory table.  This will:
 
+    * Remove lines containing NA
+    * Remove fractional time from the timestamp
+    * Remove commas from names
+    * Remove exact duplicate rows
+    * Aggregate duplicate products (sum quantity, max retail, max case_retail)
+    * Add missing products based on previous inventory
+    * Separate the size column into container_quantity, container_volume, and container_type
+    * Map names to integers and place values in the names table
+    * Map products (name, container_quantity, container_volume, and container_type, case_pack, category)
+      to integers and place vlues in the products table
+    * Remove any rows with zero quantity
+    * Save to the inventory table
+    * Compute transactions from the previous inventory and save to the transactions table
 
-    This script may be called directly to ingest all files into the inventory table (with
-    necessary additions to products, sizes, and categories).
+    This script may be called directly to ingest all files in a specified directory
     """
 
-    def __init__(self, conn, products, sizes, categories, timestamps):
+    def __init__(self, database_transactions):
         # Maps to look up (or add new) integer values for these fields
-        self.conn = conn
-        self.products = products
-        self.sizes = sizes
-        self.categories = categories
-        self.converter = Converter(products, sizes, categories)
-        self.timestamps = timestamps
-        self.cur = self.conn.cursor()
+        self.database_transactions = database_transactions
+        products_df = database_transactions.get_table('products')
+        self.products = ProductMap(products_df)
+        names_df = database_transactions.get_table('names')
+        names = NameMap(names_df)
+        self.names = names
+        current_csv_inventory_df = database_transactions.get_table('current_inventory')
+        self.current_csv_inventory = CurrentCSVInventory(current_csv_inventory_df)
+        self.prev_timestamp = None
 
     def ingest(self, filename):
         """
@@ -36,31 +48,99 @@ class Ingest:
         :return: None
         """
         data = pd.read_csv(filename)
-        data = self.converter.convert(data)
+        data = self.convert(data)
+        self.save_tables(data)
+        self.compute_and_save_transactions(data)
 
+    def convert(self, data):
+        # Rename the columns to match the database values (remove spaces, etc.)
+        name_map = {'Name': 'name',
+                    'Quantity_Available': 'quantity',
+                    'Retail': 'retail',
+                    'Case Retail': 'case_retail',
+                    'Case Pack': 'case_pack',
+                    'Category': 'category'}
+        data = data.rename(columns=name_map)
+
+        # if the file contains any incomplete lines, drop them
+        # Done in place so we don't generate SettingWithCopyWarning on later steps
+        data.dropna(inplace=True)
+        # If there is a bad line in the file, the type of 'Case Pack' will end up
+        # as a float, and writing to the DB will fail.
+        data['case_pack'] = data['case_pack'].astype(int)
+        # Remove fractional part from timestamp
+        data['timestamp'] = data['timestamp'].apply(lambda x: x.split('.')[0])
+        # Remove commas from the name
+        data['name'] = data['name'].apply(lambda x: x.replace(',', ''))
+
+        # Remove rows that duplicate the quantity and prices
+        data.drop_duplicates(subset=['name', 'size', 'category', 'case_pack', 'quantity', 'retail', 'case_retail'], inplace=True)
+        # Aggregate remaining duplicates by adding quantities and taking maximum of retail and case_retail
+        data = data.groupby(['name', 'size', 'category', 'case_pack', 'timestamp'],
+                            as_index=False).agg({'quantity': 'sum', 'retail': 'max', 'case_retail': 'max'})
+
+        # Update the current inventory in CSV format
+        data = self.current_csv_inventory.update_inventory(data)
+        # Split the size column into the quantity, volume, and type of container
+        data[['container_quantity', 'container_volume', 'container_type']] = data['size'].apply(
+            lambda x: pd.Series(parse_size(x)))
+        # Apply the name map
+        data['name_id'] = data['name'].apply(lambda x: self.names.get_value(x))
+        # The product_id is computed using multiple values from a row.  axis=1 tells Pandas to
+        # pass the entire row to the function.  We pass the timestamp in case a new value is added
+        # The DB table holds the date the product first appeared.
+        data['product_id'] = data.apply(
+            lambda row: self.products.get_value(row['name_id'], row['container_quantity'], row['case_pack'],
+                                                row['container_volume'], row['container_type'],
+                                                row['category'], row['timestamp']),
+            axis=1)
+
+        # Make the table hold only the columns we want (and in the right order)
+        columns = ['product_id', 'quantity', 'retail', 'case_retail', 'timestamp']
+        data = data[columns]
+
+        # Remove records with zero quantity (negative is kept)
+        return data[data['quantity'] != 0]
+
+    def save_tables(self, data):
         # We have to write this before the inventory data because inventory(timestamp)
         # depends on timestamps(timestamp).
-        timestamp = data['timestamp'].unique()[0]
-        self.timestamps.add(timestamp)
+        timestamp = data['timestamp'].drop_duplicates()
+        self.database_transactions.add_to_table('timestamps', timestamp, ['timestamp'])
+        names_columns = ['name_id', 'tanczos_name']
+        self.database_transactions.add_to_table('names', self.names.get_new_names(), names_columns)
+        product_columns = ['product_id', 'name_id', 'container_quantity', 'quantity_in_case', 'container_volume',
+                   'container_type', 'tanczos_category', 'date_added']
+        self.database_transactions.add_to_table('products', self.products.get_new_products(), product_columns)
 
-        # We save newly discovered keys for the same reason as the timestamp: inventory
-        # depends on the values
-        self.products.save_new_keys()
-        self.sizes.save_new_keys()
-        self.categories.save_new_keys()
+        csv_inventory_columns = ('name', 'size', 'category', 'quantity', 'retail',
+                                 'case_retail', 'case_pack', 'timestamp')
+        self.database_transactions.delete_all_rows('current_inventory')
 
-        # Convert dataframe to csv as a string
-        output = io.StringIO()
-        data.to_csv(output, header=False, index=False)
-        # seek to the beginning so copy_from can read it
-        output.seek(0)
+        self.database_transactions.add_to_table('current_inventory', self.current_csv_inventory.get_current_inventory(),
+                                                csv_inventory_columns)
+        inventory_columns = ('product_id', 'quantity', 'retail', 'case_retail', 'timestamp')
+        self.database_transactions.add_to_table('inventory', data, inventory_columns)
 
-        # Save the inventory
-        # column names are required here because the inventory_id will be auto-filled for each row
-        columns = ('product_id', 'category_id', 'size_id', 'case_pack', 'timestamp ',
-                   'quantity', 'retail', 'case_retail')
-        self.cur.copy_from(output, 'inventory', sep=',', columns=columns)
-        self.conn.commit()
+    def compute_and_save_transactions(self, data):
+        columns = ['pre_inventory_id', 'post_inventory_id', 'product_id',
+                   'transaction_quantity', 'timestamp', 'retail', 'case_retail']
+        current_timestamp = data['timestamp'][0]
+        if self.prev_timestamp == None:
+            self.prev_timestamp = current_timestamp;
+            # Return and empty dataframe with the proper columns
+            return pd.DataFrame(columns=columns)
+
+        # Get the previous timestamp and the inventory for that timestamp.  We will use them after
+        prev_inventory = self.database_transactions.get_inventory(self.prev_timestamp)
+        curr_inventory = self.database_transactions.get_inventory(current_timestamp)
+
+        # Compute the transactions
+        transactions = compute_transactions(prev_inventory, curr_inventory)
+
+        # there may not be transactions to save
+        if len(transactions) > 0:
+            self.database_transactions.add_to_table('transactions', transactions, columns)
 
 
 if __name__ == '__main__':
@@ -82,14 +162,8 @@ if __name__ == '__main__':
     database_name = os.getenv('DB_NAME')
 
     conn = psycopg2.connect(dbname=database_name, host=host, user=user, password=password)
-
-    # Maps to look up (or add new) integer values for these fields
-    products = KeyMap(conn, 'products')
-    sizes = KeyMap(conn, 'sizes')
-    categories = KeyMap(conn, 'categories')
-    timestamps = TimestampDatabase(conn)
-
-    i = Ingest(conn, products, sizes, categories, timestamps)
+    database_transactions = DBTransactions(conn)
+    i = Ingest(database_transactions)
 
     files = sorted(os.listdir(path))
     count = 0
@@ -98,6 +172,6 @@ if __name__ == '__main__':
     for file in files:
         if not file.endswith('csv'):
             continue
-        print('{}/{} ({}) {}'.format(count, total, count/total, file))
+        print('{}/{} ({}) {}'.format(count + 1, total, count/total, file))
         count += 1
         i.ingest(path + '/' + file)
