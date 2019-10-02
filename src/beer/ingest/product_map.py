5d59ee55310ff5a64031e4a571b75b2bc0d30c5b
Ben Coleman <colemanb@moravian.edu>
2019-10-01 21:29:12 -0400

    Factor out database interaction

--- src/beer/ingest/product_map.py
+++ src/beer/ingest/product_map.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 class ProductMap:
     """
     Manages the products and creates a unique key for each.
@@ -7,33 +10,40 @@ class ProductMap:
 
     """
 
-    def __init__(self, conn):
-        self.conn = conn
-        self.cur = self.conn.cursor()
-
-        self.key_map = {}
-
-        self.cur.execute('SELECT * FROM products')
-
-        for row in self.cur.fetchall():
-            product_id = row[0]
-            values = row[1:]
-            self.key_map[values] = product_id
-
+    def __init__(self, current_products):
+        if len(current_products) > 0:
+            self.key_map = {}
+            current_products.apply(lambda row: add_to_dict(self.key_map, (
+                row.name_id, row.container_quantity, row.quantity_in_case, row.container_volume,
+                row.container_type, row.tanczos_category), row.product_id), axis=1)
+        else:
+            self.key_map = {}
+        self.new_keys = {}
         self.next_id = max(self.key_map.values(), default=0) + 1
 
     def get_value(self, name_id, container_quantity, quantity_in_case,
-                  container_volume, container_type, tanczos_category):
+                  container_volume, container_type, tanczos_category, timestamp):
         key = (name_id, container_quantity, quantity_in_case, container_volume, container_type, tanczos_category)
 
         if key not in self.key_map:
             self.key_map[key] = self.next_id
-
-            self.cur.execute('INSERT INTO products VALUES(%s, %s, %s, %s, %s, %s, %s',
-                             (self.next_id, name_id, container_quantity, quantity_in_case,
-                              container_volume, container_type, tanczos_category))
-            self.conn.commit()
-
+            self.new_keys[key] = (self.next_id, timestamp)
             self.next_id += 1
 
         return self.key_map[key]
+
+    def get_new_products(self):
+        values = pd.DataFrame.from_records(list(self.new_keys.keys()),
+                                           columns=['name_id', 'container_quantity', 'quantity_in_case',
+                                                    'container_volume', 'container_type', 'tanczos_category'])
+        keys = pd.DataFrame.from_records(list(self.new_keys.values()), columns=['product_id', 'timestamp'])
+        values['product_id'] = keys['product_id']
+        values['timestamp'] = keys['timestamp']
+        columns = ['product_id', 'name_id', 'container_quantity', 'quantity_in_case', 'container_volume',
+                   'container_type', 'tanczos_category', 'timestamp']
+        self.new_keys = {}
+        return values[columns]
+
+
+def add_to_dict(d, k, v):
+    d[k] = v
