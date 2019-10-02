a496cc72c6561389afcaab3a806b4cad940c936d
Ben Coleman <colemanb@moravian.edu>
2019-10-02 00:41:42 -0400

    Reset index to avoid duplicate values when filling

--- src/beer/ingest/current_csv_inventory.py
+++ src/beer/ingest/current_csv_inventory.py
@@ -30,29 +30,16 @@ class CurrentCSVInventory:
         timestamp = df['timestamp'][0]
 
         # Concat preserves order, which we need for drop_duplicates to work
-        # reset_index renumbers the indexes to 0, 1, 2, ...  necessary for testing
+        # reset_index renumbers the indexes to 0, 1, 2, ...
         self.prev = pd.concat([self.prev, df], sort=False) \
-            .drop_duplicates(subset=['name', 'size', 'category', 'case_pack'], keep='last')
-            # Force filled timestamps to match the rest of the dataframe
-        # i.e. assume the inventory is unchanged from previous
-        self.prev['timestamp'] = timestamp
-        columns = ['name', 'size', 'category', 'quantity', 'retail', 'case_retail', 'case_pack', 'timestamp']
-        return self.prev[columns].copy()
-
-        # All values in the CSV are the same, so grab the first one
-        timestamp = df['timestamp'][0]
-
-        merged_df = pd.concat([self.prev, df], sort=False)
-
-        idx = merged_df.groupby(['name', 'size', 'category', 'case_pack']).transform(max)['timestamp'] == merged_df[
-            'timestamp']
-        self.prev = merged_df[idx].reset_index(drop=True).copy()
+            .drop_duplicates(subset=['name', 'size', 'category', 'case_pack'], keep='last') \
+            .reset_index(drop=True)
 
         # Force filled timestamps to match the rest of the dataframe
         # i.e. assume the inventory is unchanged from previous
         self.prev['timestamp'] = timestamp
-        columns = ['name','size','category','quantity','retail','case_retail','case_pack','timestamp']
-        return self.prev[columns]
+        columns = ['name', 'size', 'category', 'quantity', 'retail', 'case_retail', 'case_pack', 'timestamp']
+        return self.prev[columns].copy()
 
     def get_current_inventory(self):
         columns=['name','size','category','quantity','retail','case_retail','case_pack','timestamp']
